6dbf7c5414b0e80e4fa1c4d17166a2af1b3d9a86
Shuja2 <shuja201234@gmail.com>
2024-07-31 14:21:45 -0400

    Added ternary operator

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -42,3 +42,6 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
