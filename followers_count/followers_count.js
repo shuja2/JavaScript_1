74884e18c5db2d4fbc884ad99f4fc8eb2ceeb080
Shuja2 <shuja201234@gmail.com>
2024-08-01 02:53:29 -0400

    updted for reset btn

--- followers_count/followers_count.js
+++ followers_count/followers_count.js
@@ -17,3 +17,8 @@ function checkCountValue() {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+function reset_followers() {
+    count = 0;
+    displayCount();
+    alert("Followers count has been reset");
+}
